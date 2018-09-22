52cd9dcbf47345d7e02f112d7f3dbe93e7d127f3
Matthew Nowakowski <mjnowakowski@email.neit.edu>
2018-09-22 04:47:40 -0800

    Added access controls to Drawable

--- engine/drawable.js
+++ engine/drawable.js
@@ -1,12 +1,20 @@
 class Drawable {
     constructor(methods) {
-        this.methodList = methods;
-        this.path = new Path2D();
-        for(let i = 0; i < this.methodList.length; i++) {
-            let method = this.methodList[i];
-            this.path[method.methodName].apply(this.path, method.args)
+        let methodList = methods;
+        this.getMethodList = function() { return methodList; }
+
+        let path = new Path2D();
+        for(let i = 0; i < methodList.length; i++) {
+            let method = methodList[i];
+            path[method.methodName].apply(path, method.args)
         }
-        this.registration = new Point(0,0)
-        this.subLayers = new Array();
+        this.getPath = function() { return path; }
+
+        let registration = new Point(0,0)
+        this.getRegistration = function() { return registration; }
+        this.setRegistration = function(position) { registration = position; }
+
+        let subLayers = new Array();
+        this.getSubLayers = function() { return subLayers; }
     }
 }
--- engine/frame.js
+++ engine/frame.js
@@ -2,10 +2,10 @@ class Frame {
     constructor() {
         let onScreen = new Array();
         this.getOnScreen = function() { return onScreen; }
-        this.addObjectToFrame = function(screenObject) {
+        this.addObject = function(screenObject) {
             onScreen.push(screenObject);
         }
-        this.removeObjectFromFrame = function(screenObject) {
+        this.removeObject = function(screenObject) {
             let ndx = onScreen.indexOf(screenObject);
             if(ndx !== -1) onScreen.splice(ndx, 1);
         }
