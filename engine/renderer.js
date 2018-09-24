1afbab3ab4b04d55521da8c9e61f2333dce4edcf
Matthew Nowakowski <mjnowakowski@email.neit.edu>
2018-09-23 20:13:26 -0800

    Fixed rendering loop to only render current frame objects

--- engine/renderer.js
+++ engine/renderer.js
@@ -7,23 +7,27 @@ class Renderer {
                 Renderer._oldTime = newTime - (Renderer._elapsedTime % Renderer._fpsInterval);
                 Renderer.scrubFrames(1);
             }
-        } else dispatchEvent(Renderer._frameEvent);
-
-        let animLayers = Renderer.anim.getLayers();
-        for(let i = 0; i < animLayers.length; i++) {
-            let frames = animLayers[i].getFrames();
-            for(let j = 0; j < frames.length; j++) {
-                if(!frames[j]) continue;
-                let onScreen = frames[j].getOnScreen();
-                for(let k = 0; k < onScreen.length; k++) {
-                    Renderer.draw(onScreen[k]);
-                }
-            }
         }
+        
+        let c = Renderer.ctx.canvas;
+        Renderer.ctx.clearRect(0, 0, c.width, c.height);
+        dispatchEvent(Renderer._frameEvent);
+
+        Renderer.drawLayers(Renderer.anim, Renderer._currentFrame);
 
         if(!Renderer.playMode) dispatchEvent(Renderer._frameEndEvent);
-
         Renderer._requestId = requestAnimationFrame(Renderer.render);
+    }
+
+    static drawLayers(root, frameNdx) {
+        let layers = root.getLayers();
+        for(let i = 0; i < layers.length; i++) {
+            let frames = layers[i].getFrames();
+            if(!frames[frameNdx]) continue;
+
+            let onScreen = frames[frameNdx].getOnScreen();
+            for(let j = 0; j < onScreen.length; j++) Renderer.draw(onScreen[j]);
+        }
     }
 
     static draw(screenObject) {
@@ -46,9 +50,9 @@ class Renderer {
     }
 
     static scrubFrames(numFrames) {
-        let numAnimFrames = Renderer.anim.frames.length;
+        let numAnimFrames = Renderer.anim.getNumFrames();
         if(Renderer._currentFrame + numFrames >= 0) {
-            if(Renderer._currentFrame + numFrames < numAnimFrames) Renderer._currentFrame += speed;
+            if(Renderer._currentFrame + numFrames < numAnimFrames) Renderer._currentFrame += numFrames;
             else Renderer._currentFrame = 0;
         } else Renderer._currentFrame = numAnimFrames - 1;
     }
