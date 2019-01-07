97572420272e594819d1b33f14387a0fad3fd603
Matthew Nowakowski <mjnowakowski@email.neit.edu>
2019-01-07 08:50:13 -0500

    q

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-const { settings, env} = require('./config.js');
-const TCPServer = require('./lib/server/tcpserver.js');
+const Renderer = require("./lib/engine/module.js").Renderer;
 
-let s = new TCPServer();
-
-function closeHandler() {
-	console.log("Server ending");
-	s.stopServer();
-	process.exit();
-}
-process.on('SIGHUP', closeHandler);
-process.on('SIGINT', closeHandler);
-
-s.startServer(settings.app.port);
+let r = new Renderer(null);
