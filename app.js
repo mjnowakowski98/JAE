b052cfa137010cf14a74dc45b5fe97512a82db45
Nowakowski <mjnowakowski@labs.neit.edu>
2019-01-08 14:00:13 -0500

    send help

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const Renderer = require("./lib/engine/module.js").Renderer;
-const createCanvas = require("canvas").createCanvas;
+const { Renderer } = require("./lib/engine/module.js");
+const { createCanvas } = require("canvas");
 const canvas = createCanvas(800, 600);
 const ctx = canvas.getContext('2d');
 
 let r = new Renderer(ctx);
-
+r.getAnimation().getObjectManager().createLayer(null);
