f7c2a87e7a20be80d4ff1bcaf9030de8c8e657c3
Nowakowski <mjnowakowski@labs.neit.edu>
2019-01-09 12:32:46 -0500

    Figured out what I'm doing with this

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
+// Temporary test driver for engine
+
 const { Renderer } = require("./lib/engine/module.js");
 const { createCanvas } = require("canvas");
 const canvas = createCanvas(800, 600);
 const ctx = canvas.getContext('2d');
 
+const Layer = require("./lib/engine/layer.js");
+
 let r = new Renderer(ctx);
-r.getAnimation().getObjectManager().createLayer(null);
+let anim = r.getAnimation();
+let objMan = anim.getObjectManager();
+
+let layers = [new Layer(null), new Layer(null), new Layer(null)];
+let layerKeys = new Array();
+for(let i = 0; i < layers.length; i++) {
+    layerKeys.push(layers[i].getKey());
+    objMan.mapObject(layers[i]);
+}
+
+objMan.markForDelete(layerKeys[1]);
+objMan.cullKeyMap();
--- config.js
+++ config.js
@@ -4,11 +4,26 @@ const settings = {
 	canvas: {
 		defaultWidth: parseInt(process.env.JAE_CANVAS_DEFAULTWIDTH) || 1280,
 		defaultHeight: parseInt(process.env.JAE_CANVAS_DEFAULTHEIGHT) || 720
+	},
+	app: {
+		keyGen: {
+			charRanges: [
+				{
+					low:48,
+					high:57
+				},
+				{
+					low:97,
+					high:122
+				}
+			],
+			length:25
+		}
 	}
 };
 
 const dev = {
-
+	
 };
 
 
