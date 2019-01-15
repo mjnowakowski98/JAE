a50dacb9d6eb983ff7c317d08dd17e4dc246c816
Matthew Nowakowski <mjnowakowski@email.neit.edu>
2019-01-14 21:57:05 -0500

    Broke the engine commands

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
 // Temporary test driver for engine
 
-const { Renderer } = require("./lib/engine/module.js");
+const { Renderer, CommandHandler } = require("./lib/engine/module.js");
 const { createCanvas } = require("canvas");
 const canvas = createCanvas(800, 600);
 const ctx = canvas.getContext('2d');
 
-const Layer = require("./lib/engine/layer.js");
-
 let r = new Renderer(ctx);
 let anim = r.getAnimation();
 let objMan = anim.getObjectManager();
+let c = new CommandHandler();
 
-let layers = [new Layer(null), new Layer(null), new Layer(null)];
-let layerKeys = new Array();
-for(let i = 0; i < layers.length; i++) {
-    layerKeys.push(layers[i].getKey());
-    objMan.mapObject(layers[i]);
-}
+let commIn = { objMan:objMan, commandName:"createFrame", constructorArgs: { containerKey:anim.getKey(), name:"test" } };
+c.queueCommand(commIn);
 
-objMan.markForDelete(layerKeys[1]);
-objMan.cullKeyMap();
+c.executeNextCommand();
