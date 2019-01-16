cf48ad9aa1439730af36ab824025289450de2c96
Matthew Nowakowski <mjnowakowski@email.neit.edu>
2019-01-16 13:59:49 -0500

    Various tests

--- app.js
+++ app.js
@@ -1,19 +1,7 @@
 // Temporary test driver for engine
 
-const { Renderer, CommandHandler } = require("./lib/engine/module.js");
-const { createCanvas } = require("canvas");
-const canvas = createCanvas(800, 600);
-const ctx = canvas.getContext('2d');
-
-let r = new Renderer(ctx);
-let anim = r.getAnimation();
-let objMan = anim.getObjectManager();
-let c = new CommandHandler();
-
-let commIn = { objMan:objMan, commandName:"createLayer", constructorArgs: { containerKey:anim.getKey(), name:"test" } };
-c.queueCommand(commIn);
-console.log(c.executeNextCommand());
-
+const { settings } = require("./config.js");
 const { Server } = require("./lib/netclient/module.js");
+
 let s = new Server();
-s.startServer();
+s.startServer(settings.app.server.httpPort);
