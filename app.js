8d3cc16f3d2b3b3df128175ab8c1c3abee43e78e
Matthew Nowakowski <mjnowakowski@email.neit.edu>
2019-02-03 13:54:58 -0500

    Commented all netclient module classes Cleaned up code to end client sessions Misc refactoring

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-// Temporary test driver for engine
+const { settings } = require("./config.js"); // Get settings object from config.js
+const Server = require("./lib/netclient/module.js");
 
-const { settings } = require("./config.js");
-const { Server } = require("./lib/netclient/module.js");
-
-let s = new Server();
-s.startServer(settings.app.server.httpPort);
+let s = new Server(); // Create a new webserver instance
+s.startServer(settings.app.server.httpPort); // Start webserver
