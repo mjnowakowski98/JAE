2d69daec2c6829d03a08174c71385323a99c63f9
Matthew Nowakowski <mjnowakowski@email.neit.edu>
2019-02-19 17:21:17 -0500

    Restructured command module(s) to better allow for expansion

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const { settings } = require("./config.js"); // Get settings object from config.js
-const Server = require("./lib/netclient/module.js");
+const { Server } = require("./lib/netclient/module.js");
 
 let s = new Server(); // Create a new webserver instance
 s.startServer(settings.app.server.httpPort); // Start webserver
