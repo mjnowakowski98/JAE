c38193841bdb61e3b9ddbcf0da81df0649c2e5c7
Matthew Nowakowski <mjnowakowski@email.neit.edu>
2019-03-24 10:33:19 -0400

    new server init

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-const { settings } = require("./config.js"); // Get settings object from config.js
-const { Server } = require("./lib/netclient/module.js");
+// Application entry point
 
-let s = new Server(); // Create a new webserver instance
-s.startServer(settings.app.server.httpPort); // Start webserver
+global.appConfig = require("./config.js"); // Make appConfig global
+
+// TMP: Start a TcpSever
+const TcpServer = require("./lib/server/tcpserver.js");
+let s = new TcpServer();
+s.start(23);
--- config.js
+++ config.js
@@ -1,38 +1,25 @@
 const env = process.env.NODE_ENV || "dev";
 
 const settings = {
+	appDir:__dirname,
 	canvas: {
 		defaultWidth: parseInt(process.env.JAE_CANVAS_DEFAULTWIDTH) || 1280,
 		defaultHeight: parseInt(process.env.JAE_CANVAS_DEFAULTHEIGHT) || 720
-	},
-	app: {
-		keyGen: {
-			charRanges: [
-				{
-					low:48,
-					high:57
-				},
-				{
-					low:97,
-					high:122
-				}
-			],
-			length:25
-		},
-		server: {
-			httpPort: process.env.JAE_DEFAULTPORT || 80,
-			magicSocketString: process.env.JAE_MAGICSOCKETSTRING || "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
-		}
 	}
 };
 
 const dev = {
-	
+	uuidVersion:"v1"
 };
+
+const production = {
+	uuidVersion:"v4"
+}
 
 
 const config = {
-	dev
+	dev,
+	production
 };
 
 module.exports = {
